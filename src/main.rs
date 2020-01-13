bf9a0217ce945bf3306b6bd23d57e2b619738b37
Addison Heavner <aaheavner@mix.wvu.edu>
2020-01-13 17:37:45 -0500

    Getting started. Trying to decode torrent file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+use std::fs::File;
+use std::env;
+
+use bendy::decoding::{Error, FromBencode};
+mod bencode;
+mod tracker;
+
+fn main() -> Result<(), Error> {
+    let args: Vec<String> = env::args().collect();
+    let mut file = File::open(&args[1])?;
+    
+    let mut bytes: Vec<u8> = Vec::new();
+    file.read_to_end(&mut bytes)?;
+    
+    let torrent = bencode::MetaInfo::from_bencode(&bytes)?;
+    let url = tracker::get_url(&torrent.announce);
+    println!("{:#?}", &torrent);
+    println!("{:#?}", &url);
+
+    Ok(())
 }
---
+++ src/tracker.rs
@@ -0,0 +1,19 @@
+use std::net::{UdpSocket, SocketAddr, ToSocketAddrs};
+use url::{Url};
+
+pub fn get_url(url: &str) -> SocketAddr {
+    let parsed_url = Url::parse(&url).unwrap();
+    let host_port = format!("{}:{}", &parsed_url.host_str().unwrap(), &parsed_url.port().unwrap());
+    let mut address_iter = host_port.to_socket_addrs().unwrap();
+    dbg!(&address_iter);
+    let tracker_url = address_iter.next().unwrap();
+    tracker_url
+}
+
+pub fn contact_tracker(url: &str) {
+    
+}
+
+//get information from tracker
+    //using udp library
+
